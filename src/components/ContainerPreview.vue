aa831a8b3169934da9d2a85687894c4402194eaf
CJ Mcclure <callum.mcclure98@gmail.com>
2021-02-13 21:53:46 +0000

    hide animations

--- src/components/ContainerPreview.vue
+++ src/components/ContainerPreview.vue
@@ -1,5 +1,5 @@
 <template>
-  <div id="container" :style="alterPostion"></div>
+  <div class="vessel" :style="alterPostion"></div>
 </template>
 <script lang="ts">
 import Vue from "vue"
@@ -15,22 +15,31 @@ export default class ContainerPreview extends Vue {
     const multiplier = 0.7
     const skew = -53
     const val = skew + this.value * multiplier
-    return `background-position: 0px ${val}%`
+    return `background-position-y: ${val}%`
   }
 }
 </script>
 
 <style lang="sass">
 
-#container
+.vessel
     border: solid 1px black
     height: 100%
     width: 100%
     top: 40%
     left: 40%
     box-shadow: 0 0 0 6px #fff
-    border-radius: 0 0 20px 20px
+    border-radius: 0 0 30px 30px
     background: url(./../assets/coffee.png)
-    background-repeat: no-repeat
-    // background-position: 0px 18%
+    background-repeat: repeat-x
+    // animation: flow 25s infinite
+    // animation-direction: alternate
+    // transition-timing-function: ease-in-out
+
+@keyframes flow
+    0%
+        background-position-x: 0%
+
+    100%
+        background-position-x : 200%
 </style>
