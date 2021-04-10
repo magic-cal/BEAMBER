d68c5093fea9685d21c49670b56cb03e180e1728
CJ Mcclure <callum.mcclure98@gmail.com>
2021-04-10 14:06:48 +0100

    Add quick Recipe Add

--- server/services/DataService.ts
+++ server/services/DataService.ts
@@ -1,11 +1,28 @@
 import Guid from "../../utils/classes/common/guid"
 import { Lease } from "../../utils/classes/leases"
-import { AssemblyController } from "./AssemblyService"
-import { AssemblyStepController } from "./AssemblyStepService"
-import { LeaseController } from "./LeaseService"
-import { RecipeController } from "./RecipeService"
-import { RecipeStepController } from "./RecipeStepService"
 import { Body, Controller, Delete, Post, Put, Route, Tags } from "tsoa"
+import { locales } from "moment"
+import { Recipe } from "../../utils/classes/recipes"
+import { RecipeStep } from "../../utils/classes/recipeSteps"
+import {
+  AssemblyController,
+  AssemblyStepController,
+  LeaseController,
+  RecipeController,
+  RecipeStepController
+} from "./services"
+
+export interface RecipeBreakdownSteps {
+  name?: string
+  description?: string
+  duration?: number
+}
+
+export interface RecipeBreakdown {
+  name?: string
+  description?: string
+  breakdownSteps: RecipeBreakdownSteps[]
+}
 
 @Tags("Data")
 @Route("Data")
@@ -49,5 +66,26 @@ export class DataService extends Controller {
     )
 
     return true
+  }
+
+  @Post("create-recipes")
+  async createRecipesFromSteps(@Body() recipeBreakdown: RecipeBreakdown) {
+    const recipeId = Guid.create()
+    const recipe = new Recipe(recipeId, recipeBreakdown.name)
+    await this.recipeService.updateOrCreateRecipe(recipe)
+
+    for (const rsb of recipeBreakdown.breakdownSteps) {
+      const recipeStep = new RecipeStep(
+        Guid.create(),
+        rsb.name,
+        rsb.description,
+        undefined,
+        undefined,
+        recipeId,
+        undefined,
+        rsb.duration
+      )
+      await this.recipeStepService.updateOrCreateRecipeStep(recipeStep)
+    }
   }
 }
